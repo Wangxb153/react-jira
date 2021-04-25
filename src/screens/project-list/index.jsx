76957374bfea34bea3131069df04df98987432b2
wangxb153 <wanxgb153@chinaunicom.cn>
2021-04-25 10:14:22 +0800

    feat: custom hook包括useMount和useDebounce

--- src/screens/project-list/index.jsx
+++ src/screens/project-list/index.jsx
@@ -3,7 +3,7 @@ import { List } from './list'
 import { useEffect, useState } from "react"
 import React from 'react'
 import * as qs from 'qs'
-import { cleanObject } from 'utils'
+import { cleanObject, useMount, useDebounce } from 'utils'
 
 const apiUrl = process.env.REACT_APP_API_URL
 export const ProjectListScreen = () => {
@@ -13,22 +13,30 @@ export const ProjectListScreen = () => {
   })
   const [ list, setList ] = useState([])
   const [ users, setUsers ] = useState([])
-  
+  console.log('param',param)
+  const debouncedParam = useDebounce(param, 2000)
   useEffect(() => {
-    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(async response => {
+    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`).then(async response => {
       if(response.ok) {
         setList(await response.json())
       }
     })
-  },[param])
+  },[debouncedParam])
   
-  useEffect(() => {
+  useMount(() => {
     fetch(`${apiUrl}/users`).then(async response => {
       if(response.ok) {
         setUsers(await response.json())
       }
     })
-  }, [])
+  })
+  // useEffect(() => {
+  //   fetch(`${apiUrl}/users`).then(async response => {
+  //     if(response.ok) {
+  //       setUsers(await response.json())
+  //     }
+  //   })
+  // }, [])
   return <div>
     <SearchPanel param={param} setParam={setParam} users={users}/>
     <List list={list} users={users}/>
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react"
+
 export const isFalsy = (value) => value === 0 ? false : !value
 
 // 在一个函数里，改变传入的对象是不好的
@@ -6,13 +8,44 @@ export const cleanObject = (object) => {
   Object.keys(result).forEach(key => {
     const value = result[key]
     // if(!value) { // 当value为0的时候，不应该进入到判断里面
-    if(isFalsy(value)) {
+    if(isFalsy(value)) { // 当value是null或者undefined，空值的时候，不传
       delete result[key]
     }
   })
   return result
 }
 
-export const useMount = () => {
-  
+export const useMount = (callback) => {
+  useEffect(() => {
+    callback()
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+}
+
+// const debounce = (func, delay) => {
+//   let timeout
+//   return () => {
+//     if(timeout) {
+//       clearTimeout(timeout)
+//     }
+//     timeout = setTimeout(() => {
+//       func()
+//     }, delay)
+//   }
+// }
+// const log = debounce(() => console.log('assa'), 5000)
+// log()
+// log()
+
+export const useDebounce = (value, delay) => {
+  const [ debouncedValue, setDebouncedValue ] = useState(value)
+  console.log('value', value)
+  useEffect(() => {
+    // 每次在value变化以后，设置一个定时器
+    const timeout = setTimeout(() => setDebouncedValue(value), delay)
+    // 每次在上一个useEffect处理完以后再运行
+    return () => clearTimeout(timeout)
+  }, [value, delay])
+
+  return debouncedValue
 }
