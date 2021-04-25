01641e32b8f6ff7647412b8a09f2f4de9ec6a64f
wangxb153 <wanxgb153@chinaunicom.cn>
2021-04-25 14:05:06 +0800

    chore: 添加ts类型定义

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,21 +1,23 @@
 import { useEffect, useState } from "react"
 
-export const isFalsy = (value) => value === 0 ? false : !value
+export const isFalsy = (value: any) => value === 0 ? false : !value
 
 // 在一个函数里，改变传入的对象是不好的
-export const cleanObject = (object) => {
+export const cleanObject = (object: object) => {
   let result = {...object}
   Object.keys(result).forEach(key => {
+    // @ts-ignore
     const value = result[key]
     // if(!value) { // 当value为0的时候，不应该进入到判断里面
     if(isFalsy(value)) { // 当value是null或者undefined，空值的时候，不传
+      // @ts-ignore
       delete result[key]
     }
   })
   return result
 }
 
-export const useMount = (callback) => {
+export const useMount = (callback: () => void) => {
   useEffect(() => {
     callback()
   // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -37,7 +39,7 @@ export const useMount = (callback) => {
 // log()
 // log()
 
-export const useDebounce = (value, delay) => {
+export const useDebounce = (value: any, delay?: number) => {
   const [ debouncedValue, setDebouncedValue ] = useState(value)
   console.log('value', value)
   useEffect(() => {
