7538aca630b13e4043415fb78902c15929aa308b
wangxb153 <wanxgb153@chinaunicom.cn>
2021-04-25 15:06:35 +0800

    chore: any -> unknown 类型

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react"
 
-export const isFalsy = (value: any) => value === 0 ? false : !value
+export const isFalsy = (value: unknown) => value === 0 ? false : !value
 
 // 在一个函数里，改变传入的对象是不好的
 export const cleanObject = (object: object) => {
@@ -39,7 +39,8 @@ export const useMount = (callback: () => void) => {
 // log()
 // log()
 
-export const useDebounce = (value: any, delay?: number) => {
+// 后面用泛型来规范类型
+export const useDebounce = (value: unknown, delay?: number): any => {
   const [ debouncedValue, setDebouncedValue ] = useState(value)
   console.log('value', value)
   useEffect(() => {
