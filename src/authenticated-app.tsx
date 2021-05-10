2abfcfc2a888fa76a4ac5e887d287caae440d99e
wangxb153 <wanxgb153@chinaunicom.cn>
2021-05-10 17:25:54 +0800

    style: 用Row布局页面

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { Button } from 'antd'
 import { ProjectListScreen } from 'screens/project-list'
 import styled from '@emotion/styled'
+import { Row } from 'components/lib'
 
 // grid和flex各自的应用场景
 // 1、要考虑，是一维布局还是二维布局
@@ -15,8 +16,8 @@ import styled from '@emotion/styled'
 export const AuthenticatedApp = () => {
   const { logout } = useAuth()
   return <Container>
-    <Header>
-      <HeaderLeft>
+    <Header between={true}>
+      <HeaderLeft gap={true}>
         <h3>logo</h3>
         <h3>项目</h3>
         <h3>用户</h3>
@@ -40,34 +41,29 @@ export const AuthenticatedApp = () => {
 
 const Container = styled.div`
   display: grid;
-  grid-template-rows: 6rem 1fr 6rem;
-  grid-template-columns: 20rem 1fr 20rem;
+  grid-template-rows: 6rem calc(100vh - 6rem);
+  /* grid-template-columns: 20rem 1fr 20rem;
   grid-template-areas:
   "header header header"
   "nav main aside"
   "footer footer footer";
-  height: 100vh;
+  height: 100vh; */
   /* 每一块之间的距离 */
   /* grid-gap: 10rem; */
 `
 // grid-area 用来给grid子元素起名字
-const Header = styled.header`
-  grid-area: header;
+const Header = styled(Row)`
+  /* grid-area: header;
   display: flex;
   flex-direction: row;
   align-items:center;
-  justify-content: space-between;
+  justify-content: space-between; */
 `
-const HeaderLeft = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-around;
-  align-items: center;
-`
+const HeaderLeft = styled(Row)``;
 const HeaderRight = styled.div`
 
 `
-const Main = styled.main`grid-area: main`
+const Main = styled.main``
 /* const Nav = styled.nav`grid-area: nav`
 const Aside = styled.aside`grid-area: aside`
 const Footer = styled.footer`grid-area: footer` */
