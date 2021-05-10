a2a3dc2ef5dfa301a220f2fcefa334cab90382ce
wangxb153 <wanxgb153@chinaunicom.cn>
2021-05-10 16:29:44 +0800

    feat： 用grid和flex布局页面

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -2,11 +2,72 @@ import { useAuth } from 'context/auth-context'
 import React from 'react'
 import { Button } from 'antd'
 import { ProjectListScreen } from 'screens/project-list'
+import styled from '@emotion/styled'
 
+// grid和flex各自的应用场景
+// 1、要考虑，是一维布局还是二维布局
+// 一般来说，一维布局用flex， 二维布局用grid
+// 2、是从内容出发还是从布局出发
+// 从内容出发： 你先有一组内容（数量一般不固定），然后希望他们均匀的分布在容器中，由内容自己的大小决定占据的空间；
+// 从布局出发： 先规划网格（数量一般不固定），然后再把元素往里填充
+// 从内容出发，用flex
+// 从布局出发，用grid
 export const AuthenticatedApp = () => {
   const { logout } = useAuth()
-  return <div>
-    <Button onClick={logout}>登出</Button>
-    <ProjectListScreen/>
-  </div>
+  return <Container>
+    <Header>
+      <HeaderLeft>
+        <h3>logo</h3>
+        <h3>项目</h3>
+        <h3>用户</h3>
+      </HeaderLeft>
+      <HeaderRight>
+        <Button onClick={logout}>登出</Button>
+      </HeaderRight>
+    </Header>
+    {/* <Nav>
+      nav
+    </Nav> */}
+    <Main>
+      <ProjectListScreen/>
+    </Main>
+    {/* <Aside>
+      aside
+    </Aside>
+    <Footer>footer</Footer> */}
+  </Container>
 }
+
+const Container = styled.div`
+  display: grid;
+  grid-template-rows: 6rem 1fr 6rem;
+  grid-template-columns: 20rem 1fr 20rem;
+  grid-template-areas:
+  "header header header"
+  "nav main aside"
+  "footer footer footer";
+  height: 100vh;
+  /* 每一块之间的距离 */
+  /* grid-gap: 10rem; */
+`
+// grid-area 用来给grid子元素起名字
+const Header = styled.header`
+  grid-area: header;
+  display: flex;
+  flex-direction: row;
+  align-items:center;
+  justify-content: space-between;
+`
+const HeaderLeft = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+`
+const HeaderRight = styled.div`
+
+`
+const Main = styled.main`grid-area: main`
+/* const Nav = styled.nav`grid-area: nav`
+const Aside = styled.aside`grid-area: aside`
+const Footer = styled.footer`grid-area: footer` */
